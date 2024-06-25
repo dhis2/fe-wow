6974a6ecd69a6348682d441624659efd89110fbd
Mozafar Haider <mozafar@dhis2.org>
2024-06-25 20:59:01 +0100

    docs: update content

--- src/components/Repos/RepoBranches.tsx
+++ src/components/Repos/RepoBranches.tsx
@@ -1,5 +1,6 @@
 import { RepoType } from "@site/src/types/repos";
 import styles from "./index.module.css";
+import { Fragment } from "react";
 
 type RepoBranchesType = {
   repo: RepoType;
@@ -13,21 +14,24 @@ const RepoBranches = ({ repo }: RepoBranchesType) => {
   return (
     <>
       <div className="text--light">
-        <div>
-          {repo.branches.slice(0, 10).map((b, i) => {
-            return (
-              <>
-                <a href={`${repo.html_url}/commits/${b.name}`}>{b.name}</a>
-                {i < repo.branches.length - 1 ? ", " : <></>}
-              </>
-            );
-          })}
-        </div>
-        <div>
-          {isContinuouslyDelivered && (
+        {repo.branches?.length && (
+          <div>
+            {repo.branches.slice(0, 10).map((b, i) => {
+              return (
+                <Fragment key={b.name}>
+                  <a href={`${repo.html_url}/commits/${b.name}`}>{b.name}</a>
+                  {i < repo.branches.length - 1 ? ", " : <></>}
+                </Fragment>
+              );
+            })}
+          </div>
+        )}
+
+        {isContinuouslyDelivered && (
+          <div>
             <i className="text--danger">* this app is on Continuous Delivery</i>
-          )}
-        </div>
+          </div>
+        )}
       </div>
     </>
   );
