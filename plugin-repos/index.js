0d289c4e167d0cab2a8725bf03ec595f7fd15707
Mozafar Haider <mozafar@dhis2.org>
2024-06-26 11:48:17 +0100

    fix(plugin): move plugin to use global data to avoid issue with deployment on github

--- plugin-repos/index.js
+++ plugin-repos/index.js
@@ -11,23 +11,9 @@ export default async function myPlugin(context, options) {
       return loadRepos(options);
     },
     async contentLoaded({ content, actions }) {
-      const { createData, addRoute } = actions;
+      const { createData, addRoute, setGlobalData } = actions;
 
-      const reposJsonPath = await createData(
-        "repos.json",
-        JSON.stringify(content)
-      );
-
-      addRoute({
-        path: "/repos",
-        component: "@site/src/components/Repos/index.tsx",
-        modules: {
-          // propName -> JSON file path
-          repos: reposJsonPath,
-        },
-        exact: true,
-      });
-      /* ... */
+      setGlobalData(content);
     },
     /* other lifecycle API */
   };
